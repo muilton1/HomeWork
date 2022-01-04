3cf535a0b55296b09fce1312e612cb6cca4bde39
Pavel Stankevich <muilton19971997@tut.by>
2022-01-04 22:17:11 +0300

    add

--- HomeWork/src/home_work_3/runners/CalculatorDecoratorMain.java
+++ HomeWork/src/home_work_3/runners/CalculatorDecoratorMain.java
@@ -10,5 +10,8 @@ public class CalculatorDecoratorMain {
     public static void main(String[] args) {
         ICalculator calc = new CalculatorWithCounterAutoDecorator(new CalculatorWithMemoryDecorator(new CalculatorWithMathExtends()));
         System.out.println(calc.getAddition(4.1, calc.getAddition(calc.getMultiplication(15, 7), calc.getPow(calc.getDivision(28, 5), 2))));
+        System.out.println(((CalculatorWithCounterAutoDecorator) calc).getCountOperation());
+        //System.out.println(((CalculatorWithMemoryDecorator) calc).getResult());
     }
 }
+
